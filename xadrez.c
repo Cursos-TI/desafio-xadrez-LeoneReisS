2c34e90ac0cddeb2615448dcc1e9f188bff3b75f
Leone Reis <leonereis@hotmail.com>
2025-10-01 22:47:03 -0300

    Commit Nível Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -2,34 +2,48 @@
 
 int main() {
 
-    int movimentoTorre = 1, movimentoBispo = 1, movimentoRainha;
+    int movimentoTorre = 1, movimentoBispo = 1, movimentoRainha, movimentoCavalo = 1; // declaração de variáveis dos movimentos das peças
 
-    printf("Movimentação das peças de Xadrez \n\n");
+    printf("Movimentação das peças de Xadrez \n\n"); // título
 
-    printf("Movimentação da Torre: \n");
+    printf("Movimentação da Torre: \n"); // identificação da peça que irá movimentar
 
-        while (movimentoTorre <= 5) {
+        while (movimentoTorre <= 5) { // movimentação da torre em cinco posições para a direita
             printf("Direita \n");
             movimentoTorre++;
         }
     
     printf("\n");
 
-    printf("Movimentação do Bispo: \n");
+    printf("Movimentação do Bispo: \n"); // identificação da peça que irá movimentar
 
         do {
-            printf("Direita, Cima \n");
+            printf("Direita, Cima \n"); // movimentação do bispo em cinco posições na diagonal direita e cima
             movimentoBispo++;
         } while (movimentoBispo <= 5);
 
     printf("\n");
 
-    printf("Movimentação da Rainha: \n");
+    printf("Movimentação da Rainha: \n"); // identificação da peça que irá movimentar
 
-    for (movimentoRainha = 8; movimentoRainha > 0; movimentoRainha--) {
+    for (movimentoRainha = 8; movimentoRainha > 0; movimentoRainha--) { // movimentação da rainha em oito posições para a esquerda
         printf("Esquerda \n");
     }
         
+    printf("\n");
+
+    printf("Movimentação do Cavalo:\n"); // identificação da peça que irá movimentar
+
+    while (movimentoCavalo <= 1) { // movimentação do cavalo em uma posição para a esquerda
+
+        for (int movimentoVertical = 2; movimentoVertical >= 1; movimentoVertical--) { // movimentação do cavalo em duas posições para baixo com declaração da variável de movimento vertical
+            printf("Baixo\n");
+        }
+
+    printf("esquerda\n");
+    movimentoCavalo++;
+
+    }
 
     return 0;
 }
